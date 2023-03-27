61133e9ff7aeebfb78aa39a2bbab5edbff97074e
Rachel <rachelalberts@valkdegroot.nl>
2023-03-27 16:30:13 +0200

    fixed the bootstrap problem. Ben nu bij section 5 part 59

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -1,16 +1,39 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, map } from 'rxjs';
+import { User } from '../_models/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AccountService {
   baseUrl = 'https://localhost:7091/api/';
+  // | means its an union type. Which means it can be one or more types. It fixes the null value problem
+  private currentUserSource = new BehaviorSubject<User | null>(null);
+  //$ means its an observable
+  currentUser$ = this.currentUserSource.asObservable();
 
   constructor(private http: HttpClient) { }
 
   login(model: any)
   {
-    return this.http.post(this.baseUrl + 'account/login', model);
+    return this.http.post<User>(this.baseUrl + 'account/login', model).pipe(
+      map((respone: User) => {
+        const user = respone;
+        if(user) {
+          localStorage.setItem('user', JSON.stringify(user));
+          this.currentUserSource.next(user);
+        }
+      })
+    )
+  }
+
+  setCurrentUser(user: User){
+    this.currentUserSource.next(user);
+  }
+
+  logout() {
+    localStorage.removeItem('user');
+    this.currentUserSource.next(null);
   }
 }
