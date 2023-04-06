f88dc367dd8b1e77274e6db9ad19ad3b170a535c
Rachel <rachelalberts@valkdegroot.nl>
2023-04-06 14:45:33 +0200

    End of section 9

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -11,20 +11,14 @@ export class MembersService {
   baseUrl = environment.apiUrl;
   constructor(private http: HttpClient) { }
 
+  //Get all memebers
   getMemebers() {
-    return this.http.get<Member[]>(this.baseUrl + 'users', this.getHttpOptions())
+    return this.http.get<Member[]>(this.baseUrl + 'users')
   }
 
-  getHttpOptions() {
-    const userString = localStorage.getItem('user');
-    //if you are not logged in you wont get users
-    if(!userString) return;
-
-    const user = JSON.parse(userString);
-    return {
-      headers: new HttpHeaders({
-        Authorization: 'Bearer ' + user.token
-      })
-    }
+  //Get a single memeber
+  getMember(username:string) {
+    return this.http.get<Member>(this.baseUrl + 'users/' + username);
   }
+
 }
